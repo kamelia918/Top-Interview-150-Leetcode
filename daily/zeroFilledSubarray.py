b7d06fce2c3564ae1b39d1e6e3ec16bd419f54de
kamelia918 <kameliaremaki@gmail.com>
2025-08-19 10:01:09 +0100

    day 7 - other solution

--- daily/zeroFilledSubarray.py
+++ daily/zeroFilledSubarray.py
@@ -18,3 +18,18 @@ class Solution:
 
 
 
+#--------2nd solution 
+
+from typing import List
+
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        total = 0
+        count = 0
+        for num in nums:
+            if num == 0:
+                count += 1
+                total += count
+            else:
+                count = 0
+        return total
